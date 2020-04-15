66f950cdbc4b7ecb8fa6e215e6b4365d2b5a3ee3
Yusuf Berkay Girgin <35403526+ybgirgin3@users.noreply.github.com>
2020-04-15 15:28:48 +0300

    check internet for possible network drops

---
+++ connectionControl.py
@@ -0,0 +1,20 @@
+# !/usr/bin/python3
+# Filename connectionControl.py
+import os
+import colorama
+from colorama import Style, Fore
+import requests as req
+
+
+def chknet():
+    colorama.init()
+    url = 'https://api.binance.com/api/v3/ticker/price'
+    timeout = 5
+
+    resp = req.get(url, timeout=timeout)
+    print(Fore.GREEN + f'{resp.status_code}')
+    print(Style.RESET_ALL)
+    return True
+
+# end file
+# print(chknet())
